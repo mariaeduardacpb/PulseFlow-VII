60c78b0cd8e4af6d0946f494ed23bf258416d8c2
mariaeduardacpb <maria.ecpb@puccampinas.edu.br>
2025-04-27 15:35:05 -0300

    Tela anexo exame

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ import connectDB from './config/db.js';
 import enxaquecaRoutes from './routes/enxaquecaRoutes.js';
 import pacienteRoutes from './routes/pacienteAuthRoutes.js';
 import insoniaRoutes from './routes/insoniaRoutes.js';
+import anexoExameRoutes from './routes/anexoExameRoutes.js';
 
 dotenv.config();
 
@@ -26,12 +27,16 @@ connectDB();
 
 app.use('/client', express.static(path.join(__dirname, '..', 'client')));
 
+// Servir arquivos estáticos corretamente
+//app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 // Rotas de autenticação
 app.use('/api/auth', authRoutes);
 
 app.use('/api/enxaqueca', enxaquecaRoutes);
 app.use('/api/paciente', pacienteRoutes); // para login e registro do paciente
 app.use('/api/insonia', insoniaRoutes);
+app.use('/api/anexoExame', anexoExameRoutes);
 
 
 export default app;
