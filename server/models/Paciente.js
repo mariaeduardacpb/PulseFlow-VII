8705f5f405b511eb815cdb40b06c0a2e034a15bd
Henrique Ribeiro <henrique@proansi.com.br>
2025-05-07 00:30:39 -0300

    Ajustes backend paciente

--- server/models/Paciente.js
+++ server/models/Paciente.js
@@ -3,6 +3,13 @@ import mongoose from 'mongoose';
 const pacienteSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   cpf: { type: String, unique: true, required: true },
+  altura: { type: String, unique: true, required: true },
+  dataNascimento: { type: String, unique: true, required: true },
+  genero: { type: String, unique: true, required: true },
+  nacionalidade: { type: String, required: true },
+  peso: { type: String, required: true },
+  profissao: { type: String, required: true },
+  telefone: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   senha: { type: String, required: true },
 });
