7980f124ec937388c39defe0d9729002f26a1fe6
Henrique Ribeiro <henrique@proansi.com.br>
2025-06-16 19:37:05 -0300

    Paciente

--- server/models/Paciente.js
+++ server/models/Paciente.js
@@ -2,18 +2,18 @@ import mongoose from 'mongoose';
 
 const pacienteSchema = new mongoose.Schema({
   nome: { type: String, required: true },
-  fotoPerfil: {type: String, required: true },
+  fotoPerfil: { type: String, default: '/client/public/assets/User_logonegativo.png' },
   cpf: { type: String, unique: true, required: true },
-  altura: { type: String, unique: true, required: true },
-  dataNascimento: { type: String, unique: true, required: true },
-  genero: { type: String, unique: true, required: true },
+  altura: { type: String, required: true },
+  dataNascimento: { type: String, required: true },
+  genero: { type: String, required: true },
   nacionalidade: { type: String, required: true },
   peso: { type: String, required: true },
   profissao: { type: String, required: true },
   telefone: { type: String, required: true },
   email: { type: String, unique: true, required: true },
   senha: { type: String, required: true },
-  observacoes: { type: String, required: true },
+  observacoes: { type: String, default: 'Nenhuma observação registrada' }
 });
 
 const Paciente = mongoose.model('Paciente', pacienteSchema, 'pacientes');
